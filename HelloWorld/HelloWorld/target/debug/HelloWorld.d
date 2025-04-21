d7545bd3343c32cd6d5a5a8941f9f5e635e8e838
Otinpan <hasesyou7531@gmail.com>
2025-04-21 16:33:30 +0900

    HelloWorld

---
+++ HelloWorld/HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+/home/hase/Rust/RustGit/HelloWorld/HelloWorld/target/debug/HelloWorld: /home/hase/Rust/RustGit/HelloWorld/HelloWorld/src/main.rs
---
+++ HelloWorld/HelloWorld/target/debug/deps/HelloWorld-58a35f04c18119e0.d
@@ -0,0 +1,5 @@
+/home/hase/Rust/RustGit/HelloWorld/HelloWorld/target/debug/deps/HelloWorld-58a35f04c18119e0: src/main.rs
+
+/home/hase/Rust/RustGit/HelloWorld/HelloWorld/target/debug/deps/HelloWorld-58a35f04c18119e0.d: src/main.rs
+
+src/main.rs:
